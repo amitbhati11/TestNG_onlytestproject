0d61379e08787a0fb992c7d9114789a2fa78aa29
Admin_SRV <Admin_SRV@Admin_SRV-PC>
2022-04-13 11:46:29 +0530

    #Jenkins#EclipseIDE#Testng

--- OnlyTestingProject/src/test/java/Listners/ListnerTest.java
+++ OnlyTestingProject/src/test/java/Listners/ListnerTest.java
@@ -1,7 +1,13 @@
 package Listners;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.IClass;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
@@ -11,7 +17,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.Test;
 
 public class ListnerTest implements ITestListener {
-	
+	WebDriver driver;
 	
 		
 	    public void onFinish(ITestContext context) {					
@@ -32,7 +38,19 @@ public class ListnerTest implements ITestListener {
 
 	 		
 	    public void onTestFailure(ITestResult result) {					
-                System.out.println("******test2"+result.getName());	        		
+                System.out.println("******test2"+result.getName());	
+                
+                TakesScreenshot srcshot= (TakesScreenshot)driver;
+                File srcfile=srcshot.getScreenshotAs(OutputType.FILE);
+                File destfile=new File("C:\\Users\\Admin_SRV\\git\\TestNG_onlytestproject\\OnlyTestingProject\\target");
+                
+                try {
+					FileUtils.copyDirectory(srcfile, destfile);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.out.println();
+				}
 	    }		
 
 			
